d6a52d5b38acd59469c6550c0287f9d32fe212e3
Rox7458 <jagaaoguz.7000@gmail.com>
2025-05-06 13:43:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header";
+import Card from "./components/products/Card";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Card />
+    </>
+  );
+}
+
+export default App;
